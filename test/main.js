760ec7c7295895e19001a986c7b9da639b19c3ca
ayhankuru <ayhankuru@yandex.com.tr>
2015-04-03 18:19:08 +0300

    fix

--- test/main.js
+++ test/main.js
@@ -3,26 +3,6 @@ var fs = require('fs');
 var gutil = require('gulp-util');
 var Crass = require('../index.js');
 
-it('GulpCrass SourceMap Test', function (cb) {
-	var stream = Crass({pretty:false,sourceMap:true});
-
-	stream.on('data', function (file) { 
-		expect(file.contents.toString()).to.eql(fs.readFileSync(__dirname + '/expected/index.css').toString());
-		expect(file.relative).to.eql('index.css');
-	});
-
-	stream.on('end', cb);
-
-	stream.write(new gutil.File({
-		cwd: __dirname,
-		base: __dirname + '/fixtures',
-		path: __dirname + '/fixtures/index.css',
-		contents: fs.readFileSync(__dirname + '/fixtures/index.css')
-	}));
-
-	stream.end();
-});
-
 
 it('GulpCrass Minified Test', function (cb) {
 	var stream = Crass({pretty:false});
